199ab35a85563911c69d90147c2f1f01fe06e8c2
mghilardelli <marco@gawnet.ch>
2016-05-09 22:36:11 +0200

    made new class Node

--- src/Huffman.java
+++ src/Huffman.java
@@ -5,29 +5,48 @@ import java.time.Clock;
  * Created by marcoghilardelli on 09.05.16.
  */
 public class Huffman {
+    public class Node {
+        public char character;
+        public int count;
+        public Node left;
+        public Node right;
+
+        public Node(char character, int count, Node left, Node right) {
+            this.character = character;
+            this.count = count;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static Node[] table = new Node[128];
+
+    public Huffman() {
+        for(int i = 0; i < table.length; i++) {
+            table[i] = new Node((char)i, 0, null, null);
+        }
+
+    }
     public static void main(String[] args) {
-        int[][] asciiTable = new int[128][2];
+
         int count = 0; // counter for ascii symbols
 
         String text = readTextfile("text.txt");
 
-
-        for(int i = 0; i < asciiTable.length; ++i) {
-            asciiTable[i][1] = 0;
-        }
+        Huffman huff = new Huffman();
         for(int i = 0; i < text.length(); ++i) {
-            asciiTable[text.charAt(i)][1]++;
-            asciiTable[text.charAt(i)][0] = text.charAt(i);
-            count++;
-            text.charAt(i);
-        }
-        for(int i = 0; i < asciiTable.length; ++i) {
-            System.out.println((char)asciiTable[i][0] + " " + asciiTable[i][1]);
-        }
-        asciiTable = sort(asciiTable);
+            for(int j = 0; j < table.length; j++) {
+                if(table[j].character == text.charAt(i)) {
+                    table[j].count++;
+                    count++;
+                }
+            }
 
-        for(int i = 0; i < asciiTable.length; ++i) {
-            System.out.println((char)asciiTable[i][0] + " " + asciiTable[i][1]);
+        }
+        table = sort(table);
+
+        for(int i = 0; i < table.length; ++i) {
+            System.out.println((char)table[i].character + " " + table[i].count);
         }
 
 
@@ -68,17 +87,14 @@ public class Huffman {
         return bFile;
 
     }
-    public static int[][] sort(int[][] array) {
-        int tempCnt, tempChar;
+    public static Node[] sort(Node[] array) {
+        Node tempNode;
         for (int i = 1; i < array.length; i++) {
             for (int j = 0; j < array.length - i; j++) {
-                if (array[j][1] > array[j + 1][1]) {
-                    tempCnt = array[j][1];
-                    tempChar = array[j][0];
-                    array[j][1] = array[j + 1][1];
-                    array[j][0] = array[j + 1][0];
-                    array[j + 1][1] = tempCnt;
-                    array[j + 1][0] = tempChar;
+                if (array[j].count > array[j + 1].count) {
+                    tempNode = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = tempNode;
                 }
             }
         }
